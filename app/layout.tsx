4fc64bc827e81fe7ac7a9b22ff29fb5229e0a15d
Lars-Ostervold <ostervold.berent@gmail.com>
2025-02-23 20:26:02 -0600

    og updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,21 @@ import { Inter } from "next/font/google"
 import type { Metadata } from "next"
 import WaterBackground from "@/components/water-background"
 import Navbar from "@/components/navbar"
-import { SITE_IMAGE, SITE_URL, SITE_DESCRIPTION, SITE_KEYWORDS } from "@/lib/constants"
+import { SITE_IMAGE, SITE_URL, SITE_DESCRIPTION, SITE_KEYWORDS, SITE_NAME, SITE_TITLE } from "@/lib/constants"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Baptized Technology - AI Refined in the Fire of Baptism",
+  title: SITE_TITLE,
   description: SITE_DESCRIPTION,
   keywords: SITE_KEYWORDS,
+  applicationName: SITE_NAME,
   openGraph: {
-    title: "Baptized Technology - AI Refined in the Fire of Baptism",
+    title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     url: SITE_URL,
     images: [SITE_IMAGE],
+    siteName: SITE_NAME,
   },
   twitter: {
     card: "summary_large_image",
