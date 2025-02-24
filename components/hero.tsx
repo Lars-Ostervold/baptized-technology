eaf0a14e1ca14fcb1be79aa9c97b29fb91c91df0
Lars-Ostervold <ostervold.berent@gmail.com>
2025-02-23 20:51:02 -0600

    fix cutoff

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <section className="container flex min-h-[calc(100vh-3.5rem)] max-w-screen-2xl flex-col items-center justify-center space-y-8 py-24 text-center md:py-32">
       <div className="space-y-4">
-        <h1 className="bg-gradient-to-br from-foreground from-30% via-foreground/90 to-foreground/70 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-5xl md:text-6xl lg:text-7xl">
+        <h1 className="bg-gradient-to-br pb-2 from-foreground from-30% via-foreground/90 to-foreground/70 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-5xl md:text-6xl lg:text-7xl">
           Technology Immersed in the Fire of Baptism
         </h1>
         <p className="mx-auto max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
